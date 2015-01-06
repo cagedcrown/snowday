bdd367a9eb280776829123f8729fb940d559dbd6
Maury Lindo <maury@cagedcrown.com>
2015-01-06 09:59:08 -0500

    add some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Snowpic.destroy_all
+Snowpic.create([
+	{ image_url: 'http://www.jacdorpreschool.co.uk/wp-content/uploads/2013/08/snow-pic.png', caption: '2 hour delay' },
+	{ image_url: 'http://www.whitegadget.com/attachments/pc-wallpapers/85680d1320642039-snow-snow-pic.jpg', caption: 'Head to the thirsty turtle' },
+])
